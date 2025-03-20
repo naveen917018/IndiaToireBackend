08b255a5b0da377e05929fee37de80741dc630ab
naveen917018 <naveen917018@gmail.com>
2025-03-20 15:02:52 +0530

    myfirstproject

---
+++ src/main/java/com/I2I/I2IBaceknd/I2IBacekndApplication.java
@@ -0,0 +1,13 @@
+package com.I2I.I2IBaceknd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I2IBacekndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I2IBacekndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/I2I/I2IBaceknd/I2IBacekndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.I2I.I2IBaceknd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I2IBacekndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
